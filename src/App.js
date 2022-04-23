cf5cb049e54e1414936e6932605c7ee9a8643b52
ahmedhammami <ahammami2016@gmail.com>
2022-04-23 10:14:31 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import mon from './mon.jpg';
+function App() {
+  return (
+    <div className="App">
+      
+      <img src={mon} className="mon"></img>
+      
+      <div className="card" id="cardd" >
+      <form className="row g-3">
+  <div className="col-md-6">
+    <label for="inputEmail4" className="form-label">Email</label>
+    <input type="email" className="form-control" id="inputEmail4"></input>
+  </div>
+  <div className="col-md-6">
+    <label for="inputPassword4" className="form-label">Password</label>
+    <input type="password" className="form-control" id="inputPassword4"></input>
+  </div>
+  <div className="col-12">
+    <label for="inputAddress" className="form-label">Address</label>
+    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"></input>
+  </div>
+  <div className="col-12">
+    <label for="inputAddress2" className="form-label">Address 2</label>
+    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
+  </div>
+  <div className="col-md-6">
+    <label for="inputCity" className="form-label">City</label>
+    <input type="text" className="form-control" id="inputCity"></input>
+  </div>
+  <div className="col-md-4">
+    <label for="inputState" className="form-label">State</label>
+    <select id="inputState" className="form-select">
+      <option selected>Choose</option>
+      <option>M</option>
+    </select>
+  </div>
+  <div className="col-md-2">
+    <label for="inputZip" className="form-label">Zip</label>
+    <input type="text" className="form-control" id="inputZip"></input>
+  </div>
+  <div className="col-12">
+    <div className="form-check">
+      <input className="form-check-input" type="checkbox" id="gridCheck"></input>
+      <label className="form-check-label" for="gridCheck">
+        Homme
+      </label>
+    </div>
+    <div className="form-check">
+      <input className="form-check-input" type="checkbox" id="gridCheck"></input>
+      <label className="form-check-label" for="gridCheck">
+        Femme
+      </label>
+    </div>
+  </div>
+  <div className="col-12">
+    <button type="submit" className="btn btn-primary">Sign in</button>
+  </div>
+</form>
+</div>
+</div>
+    
+  );
+}
+
+export default App;
